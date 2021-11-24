d9c4d53c04a645410852522e63e8e87b4c850280
lnrdgst <leo.afm@hotmail.com>
2021-11-24 20:47:06 -0300

    persistindo dados na tabela cozinha com anotation transactional

---
+++ src/main/java/com/algaworks/algafood/jpa/IncluirCozinhaMain.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafood.jpa;
+
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Cozinha;
+
+public class IncluirCozinhaMain {
+	
+	public static void main(String [] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		Cozinha coz1 = new Cozinha();
+		coz1.setNome("Árabe");
+		
+		Cozinha coz2 = new Cozinha();
+		coz2.setNome("Mexicana");
+		
+		coz1 = cadastroCozinha.adicionar(coz1);
+		coz2 = cadastroCozinha.adicionar(coz2);
+		
+		System.out.printf("%d - %s\n", coz1.getId(),  coz1.getNome());
+		System.out.printf("%d - %s\n", coz2.getId(),  coz2.getNome());
+		
+		
+				
+	}
+
+}
