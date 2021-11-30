1b0fe770d458f7f60059665755ce8fd70b4c17fe
lnrdgst <leo.afm@hotmail.com>
2021-11-29 21:20:02 -0300

    Inserindo anotacoes manytoone, joincolumn referenciando o nome do campo da tabela com nullable na anotacao column

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -5,30 +5,38 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
+@Component
 public class RestauranteRepositoryImpl implements RestauranteRepository {
 	
 	@PersistenceContext
 	private EntityManager manager;
 
 	@Override
+	@Transactional
 	public List<Restaurante> todos() {
 		return manager.createQuery("from Restaurante", Restaurante.class).getResultList();
 	}
 
 	@Override
+	@Transactional
 	public Restaurante porId(Long id) {
 		return manager.find(Restaurante.class, id);
 	}
 
 	@Override
+	@Transactional
 	public Restaurante salvar(Restaurante restaurante) {
 		return manager.merge(restaurante);
 	}
 
 	@Override
+	@Transactional
 	public void remover(Restaurante restaurante) {
 		restaurante = this.porId(restaurante.getId());
 		manager.remove(restaurante);
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,8 +4,8 @@ insert into cozinha (nome) values ('Alemã');
 insert into cozinha (nome) values ('Mexicana');
 insert into cozinha (nome) values ('Mineira');
 
-insert into restaurante (nome, taxa_frete) values ('KaiZen Sushi', 14.99);
-insert into restaurante (nome, taxa_frete) values ('ThaiLanches', 0);
-insert into restaurante (nome, taxa_frete) values ('Hailander', 20);
-insert into restaurante (nome, taxa_frete) values ('AndaLê', 15);
-insert into restaurante (nome, taxa_frete) values ('Jubeba e Limão', 9.99);
+insert into restaurante (nome, taxa_frete, cozinha_codigo) values ('KaiZen Sushi', 14.99, 1);
+insert into restaurante (nome, taxa_frete, cozinha_codigo) values ('ThaiLanches', 0, 2);
+insert into restaurante (nome, taxa_frete, cozinha_codigo) values ('Hailander', 20, 3);
+insert into restaurante (nome, taxa_frete, cozinha_codigo) values ('AndaLê', 15, 4);
+insert into restaurante (nome, taxa_frete, cozinha_codigo) values ('Jubeba e Limão', 9.99, 5);
