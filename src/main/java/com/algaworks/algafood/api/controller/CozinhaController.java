0ac9ff497448b51f444a52f7b8294cc82a1fcbc3
lnrdgst <leo.afm@hotmail.com>
2022-08-17 21:27:39 -0300

    inserindo anotacao PostMapping

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,16 +33,29 @@ public class CozinhaController {
 	
 	//@ResponseStatus(HttpStatus.CREATED)
 	@GetMapping("/{cozinhaId}")
+	
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
 		Cozinha cozinha = cozinhaRepository.porId(cozinhaId);
+		
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+		
+		return ResponseEntity.notFound().build();
 		
 		//return ResponseEntity.status(HttpStatus.OK).body(cozinha);
 		//return ResponseEntity.ok(cozinha);
 		
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+		//HttpHeaders headers = new HttpHeaders();
+		//headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
 		
-		return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+		//return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void adicionar(@RequestBody Cozinha cozinha) {
+		cozinhaRepository.salvar(cozinha);
 	}
 
 }
