8424b34aa00336a3f50e6d3d35c5a80cf964bf62
lnrdgst <leo.afm@hotmail.com>
2022-08-17 21:36:59 -0300

    removendo metodo void e inserindo return para trazer o valor do post

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -56,6 +56,7 @@ public class CozinhaController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
 		return cozinhaRepository.salvar(cozinha);
+		
 	}
 
 }
