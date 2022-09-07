2f4dbcfebc8a1c14ae473758a7669fd42345283d
lnrdgst <leo.afm@hotmail.com>
2022-09-07 20:34:48 -0300

    inserindo anotacao @DeleteMapping para a exclusao de dados juntamente com o status.CONFLICT

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -75,6 +76,22 @@ public class CozinhaController {
 			return ResponseEntity.ok(cozinha);
 		}
 		return ResponseEntity.notFound().build();
+	}
+	
+	
+	@DeleteMapping("/{cozinhaId")
+	public ResponseEntity<Cozinha> remover (@PathVariable Long cozinhaId){
+		Cozinha cozinha = cozinhaRepository.porId(cozinhaId);
+		
+		try {
+			if (cozinha != null) {
+				cozinhaRepository.remover(cozinha);
+				return ResponseEntity.noContent().build();
+			}
+			return ResponseEntity.notFound().build();
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
 	}
 
 }
